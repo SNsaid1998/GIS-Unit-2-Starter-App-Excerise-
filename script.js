37bbb46a97ee2cc923838fe5d7acd73e723e588e
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-17 18:13:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
- require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
+require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig, Map, MapView) {
+
+esriConfig.apiKey =  "AAPKdc25ad7513a24c46b92cd024b53471173ysex4xHWQwBw2IQ-kNnn5MYzM2Maqnuc7YbUYeHblfF461bYrN1hiCHw4m9SK0E"; 
+
+const map = new Map({
+          basemap: "arcgis-topographic" // Basemap layer service
         });
 
-        var view = new MapView({
-          container: "viewDiv",
+const view = new MapView({
           map: map,
-          zoom: 10,
-          center: [-90, 38.85] // longitude, latitude
+          center: [-118.805, 34.027], // Longitude, latitude
+          zoom: 13, // Zoom level
+          container: "viewDiv" // Div element
         });
-      });
+
+ });
